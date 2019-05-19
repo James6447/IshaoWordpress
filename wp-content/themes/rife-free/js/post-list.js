34c9e830425e146d460de9ee7c668c39f264f339
Liew Yee James <jamesvyean38@gmail.com>
2019-05-20 03:10:52 +0800

    finish the content feed

--- wp-content/themes/rife-free/js/post-list.js
+++ wp-content/themes/rife-free/js/post-list.js
@@ -27,16 +27,59 @@ jQuery(document).ready(function() {
     var lenght = postList.length;
 
     postList.each(function(index, element) {
-       jQuery(this).removeClass();
+        jQuery(this).removeClass();
 
-       if(index === 0) {
-           jQuery(this).addClass("col-md-12 col-sm-2 col-xs-12 pt-cv-content-item pt-cv-1-col special-post float-left");
-       }else if(index === (lenght - 1) ) {
-           jQuery(this).addClass("col-md-12 col-sm-2 col-xs-12 pt-cv-content-item pt-cv-1-col special-post float-right");
-       }else {
-               jQuery(this).addClass("col-md-3 col-sm-2 col-xs-12 pt-cv-content-item pt-cv-1-col");
-       }
+        if(index === 0) {
+            jQuery(this).addClass("col-md-12 col-sm-2 col-xs-12 pt-cv-content-item pt-cv-1-col special-post float-left");
+        }else if(index === (lenght - 1) ) {
+            jQuery(this).addClass("col-md-12 col-sm-2 col-xs-12 pt-cv-content-item pt-cv-1-col special-post float-right");
+        }else {
+            jQuery(this).addClass("col-md-3 col-sm-2 col-xs-12 pt-cv-content-item pt-cv-1-col");
+        }
 
     });
+
+    //post header remove
+    var post_meta_time = jQuery('#mid > header > div > div > div.post-meta > time')[0];
+    var post_meta_author = jQuery('#mid > header > div > div > div.post-meta > a')[0];
+    var post_footer = jQuery('#text-bottom');
+
+    // var new_meta = 'by'+ post_meta_author + post_meta_time;
+    // console.log(post_meta_author)
+    post_footer.append('By');
+    post_footer.append(post_meta_author);
+    post_footer.append('<br>');
+    post_footer.append(post_meta_time);
+    jQuery('.single-post > div > #mid > header').remove();
+
+    //work single page tags
+    var tag = jQuery('.cpt-categories.work-categories > a');
+    var show_tag = jQuery('#tags');
+    var div = '<div id="work-tag">';
+
+    jQuery(tag).each(function (index, element) {
+        div = div + '<span>' + jQuery(element).html() + '</span></br>';
+    });
+    div = div + '</div>';
+    show_tag.append(div);
+    //remove categories after replace with tag
+    jQuery('#tags > .elementor-widget-container').remove();
+    jQuery('.work-categories').remove();
+    jQuery('.cpt-nav').remove();
+
+    //customize people filter list on page '團隊'
+    var list_item = jQuery('.people-filter > li');
+
+    list_item.each(function (index, element) {
+        var li_text = jQuery(this)[0].lastChild.innerHTML;
+
+        var split_work = li_text.split('/');
+
+        if(split_work[0] === 'works') {
+            jQuery(this).remove();
+        }
+    })
+
+    jQuery('.people-template-default > div > #mid > header').remove()
 });
 
